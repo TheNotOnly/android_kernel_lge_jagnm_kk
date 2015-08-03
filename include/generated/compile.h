c3968968399c70a74e6184f1171fd1aee808b637
TheNotOnly <darren.ramen@gmail.com>
2015-08-03 21:12:34 +0400

    Initial Commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 7 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#7 SMP PREEMPT Mon Aug 3 20:59:37 MUT 2015"
+#define LINUX_COMPILE_BY "darren"
+#define LINUX_COMPILE_HOST "thenotonly"
+#define LINUX_COMPILER "gcc version 5.1.0 (crosstool-NG 1.20.0) "
