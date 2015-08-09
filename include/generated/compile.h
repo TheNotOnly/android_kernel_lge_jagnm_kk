e932ec961b9bac9ebf4e739ddaaba0c27fb9dbdf
TheNotOnly <darren.ramen@gmail.com>
2015-08-09 18:50:14 +0400

    Fix compilation errors for kernel patch 3.4.0 - 3.4.40

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 7 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#7 SMP PREEMPT Mon Aug 3 20:59:37 MUT 2015"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Aug 9 18:32:14 MUT 2015"
 #define LINUX_COMPILE_BY "darren"
 #define LINUX_COMPILE_HOST "thenotonly"
 #define LINUX_COMPILER "gcc version 5.1.0 (crosstool-NG 1.20.0) "
